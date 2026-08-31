67b1d4a10affe85e4ed767df6261faf8acdb977f
Sumedh20 <sumedhbodade7@gmail.com>
2026-08-31 20:53:30 +0530

    Fibonacci Series

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,4 +1,26 @@
 package Basics.Important;
 
-public class Fibonacci {
+import java.util.Scanner;
+
+public class Fibonacci
+{
+    public static void main(String[] args) {
+        //Printing fibonacci number series
+        int number = 5;
+        int first =1 ;
+        int second = 1;
+        System.out.print(first+" "+second+" ");
+        int third = 0;
+        int i =0;
+        while(i < number)
+        {
+            third = first + second;
+            if(third>number)
+                break;
+            System.out.print(third+" ");
+            first = second;
+            second = third;
+            i++;
+        }
+    }
 }
